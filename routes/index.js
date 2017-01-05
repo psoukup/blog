e5f322ec9467551ccd045494679175d1b5c54b14
ps <wpricesoukup@gmail.com>
2017-01-05 13:45:06 -0700

    fixed a bug with the knex file path

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     router = express.Router(),
-    knex = require('./db/knex');
+    knex = require('../db/knex');
 
 router.get('/', function(req, res) {
   res.send('This Index Route is Working')
