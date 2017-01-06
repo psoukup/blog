98e46f30a928938c3d153755de193ccc52e727c1
ps <wpricesoukup@gmail.com>
2017-01-05 23:42:39 -0700

    added views for layout and blog, and added a stylesheet

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express'),
     router = express.Router(),
     knex = require('../db/knex');
 
+//homepage or blog route
 router.get('/', function(req, res) {
   res.send('This Index Route is Working')
 });
